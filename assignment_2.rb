0502eb1bed06b7589851b6131a85420a284859ed
naveen.patil <naveen.patil@weboniselab.com>
2017-02-17 13:39:52 +0530

    Implement assignment 2 in ruby

---
+++ assignment_2.rb
@@ -0,0 +1,43 @@
+# Problem Statement
+# You are given two strings, A and B. Find if there is a substring that appears
+# in both A and B.
+# Input Format
+# Several test cases will be given to you in a single file. The first line of
+# the input will contain a single integer T, the number of test cases.
+# Then there will be T descriptions of the test cases. Each description contains
+# two lines. The first line contains the string A and the second line contains
+# the string B.
+# Output Format
+# For each test case, display YES (in a newline), if there is a common
+# substring. Otherwise, display NO.
+# Sample Input
+# 2
+# hello
+# world
+# hi
+# world
+#
+#
+# Sample Output
+# YES
+# NO
+require 'pry'
+
+class Assignment2
+  def get_num_of_test_cases
+    puts 'Enter number of test cases: '
+    gets.to_i
+  end
+
+  def evaluate_input_string
+    get_num_of_test_cases.times do
+      puts 'Enter two strings:'
+      s1 = gets.chomp
+      s2 = gets.chomp
+      s1.each_char { |c| return puts 'YES' if s2.match(c) }
+      puts 'NO'
+    end
+  end
+
+  Assignment2.new.evaluate_input_string
+end
